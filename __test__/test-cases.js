f9ae3310818df160af7322c02185534499201e00
prd-y-nguyen <y.nguyen@paradox.ai>
2022-09-06 14:21:39 +0700

    feat: stricter test

--- __test__/test-cases.js
+++ __test__/test-cases.js
@@ -1,6 +1,6 @@
 module.exports = [
-  // Most happy case
-  `Formkl {
+  [
+    `Formkl {
     "Personal Information"includes {
      "Fullname" text;
      VN phone;
@@ -10,23 +10,167 @@ module.exports = [
       "Bio" paragraph;
     }
   }`,
-  'formkl "A random survey" { "Personal Information" includes {email;}}',
-  'formkl {"Personal Information"  includes   {require email;}}',
-  'formkl { "Personal Information"    includes {require "Fullname" text;}}',
-  `formkl {
+    {
+      sections: [
+        {
+          title: "Personal Information",
+          fields: [
+            {
+              type: "text",
+              label: "Fullname",
+              require: false,
+            },
+            {
+              type: "VN phone",
+              require: false,
+            },
+          ],
+        },
+        {
+          title: "Other Information",
+          fields: [
+            {
+              type: "zip",
+              require: true,
+            },
+            {
+              type: "paragraph",
+              label: "Bio",
+              require: false,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    'formkl "A random survey" { "Personal Information" includes {email;}}',
+    {
+      title: "A random survey",
+      sections: [
+        {
+          title: "Personal Information",
+          fields: [
+            {
+              type: "email",
+              require: false,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    'formkl {"Personal Information"  includes   {require email;}}',
+    {
+      sections: [
+        {
+          title: "Personal Information",
+          fields: [
+            {
+              type: "email",
+              require: true,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    'formkl { "Personal Information"    includes {require "Fullname" text;}}',
+    {
+      sections: [
+        {
+          title: "Personal Information",
+          fields: [
+            {
+              type: "text",
+              label: "Fullname",
+              require: true,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    `formkl {
     "Personal Information" includes{
   "Fullname" text;
   US phone;
   }
 }`,
-  `formkl "Some random survey" "This form is to survey and stuff " {
+    {
+      sections: [
+        {
+          title: "Personal Information",
+          fields: [
+            {
+              type: "text",
+              label: "Fullname",
+              require: false,
+            },
+            {
+              type: "US phone",
+              require: false,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    `formkl "Some random survey" "This form is to survey and stuff " {
     "Personal Information" includes{
       "Fullname" text;
       require "Bio" paragraph regex("^[a-zA-Z]$") valid(> 100 and < 300);
       multiple "Custom regex" text regex("^[a-zA-Z]$");
     }
   }`,
-  `formkl {
+    {
+      title: "Some random survey",
+      description: "This form is to survey and stuff ",
+      sections: [
+        {
+          title: "Personal Information",
+          fields: [
+            {
+              type: "text",
+              label: "Fullname",
+              require: false,
+            },
+            {
+              type: "paragraph",
+              label: "Bio",
+              require: true,
+              validation: {
+                regex: "^[a-zA-Z]$",
+                $and: [
+                  {
+                    $gt: 100,
+                  },
+                  {
+                    $lt: 300,
+                  },
+                ],
+              },
+            },
+            {
+              type: "text",
+              label: "Custom regex",
+              require: false,
+              validation: {
+                regex: "^[a-zA-Z]$",
+              },
+              multiple: true,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    `formkl {
     multiple "Personal Information"includes {
      "Fullname" text;
       "Gender" radio ("Male","Female","Other");
@@ -43,17 +187,147 @@ module.exports = [
 
     }
   }`,
-  'formkl {"Personal Information" includes {   US phone;}}',
-  'Formkl {   "Personal Information" includes  {require US phone;   }}',
-  'formkl {"Personal Information" includes  {require time;}}',
-  'formkl {"Personal Information" includes {require "Date of birth" birthday;}}',
-  `formkl {
+    {
+      sections: [
+        {
+          title: "Personal Information",
+          multiple: true,
+          fields: [
+            {
+              type: "text",
+              label: "Fullname",
+              require: false,
+            },
+            {
+              type: "radio",
+              options: ["Male", "Female", "Other"],
+              label: "Gender",
+              require: false,
+            },
+            {
+              type: "select",
+              options: [],
+              fetchUrl: "/api/company",
+              label: "Current Company",
+              require: true,
+            },
+            {
+              type: "US phone",
+              require: false,
+            },
+          ],
+        },
+        {
+          title: "Other Information",
+          multiple: true,
+          fields: [
+            {
+              type: "zip",
+              require: true,
+              multiple: true,
+            },
+            {
+              type: "text",
+              label: "Some field",
+              require: false,
+              validation: {
+                $lt: 512,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    'formkl {"Personal Information" includes {   US phone;}}',
+    {
+      sections: [
+        {
+          title: "Personal Information",
+          fields: [
+            {
+              type: "US phone",
+              require: false,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    'Formkl {   "Personal Information" includes  {require US phone;   }}',
+    {
+      sections: [
+        {
+          title: "Personal Information",
+          fields: [
+            {
+              type: "US phone",
+              require: true,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    'formkl {"Personal Information" includes  {require time;}}',
+    {
+      sections: [
+        {
+          title: "Personal Information",
+          fields: [
+            {
+              type: "time",
+              require: true,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    'formkl {"Personal Information" includes {require "Date of birth" birthday;}}',
+    {
+      sections: [
+        {
+          title: "Personal Information",
+          fields: [
+            {
+              type: "birthday",
+              label: "Date of birth",
+              require: true,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    `formkl {
     "Re-Index User" includes {
       require "User email" email;
     }
   }
   `,
-  `formkl {
+    {
+      sections: [
+        {
+          title: "Re-Index User",
+          fields: [
+            {
+              type: "email",
+              label: "User email",
+              require: true,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    `formkl {
     "Reverting location keywords" includes {
       require "Target Company" select url("/api/company");
       require "From keyword" text;
@@ -62,7 +336,40 @@ module.exports = [
     }
   }
   `,
-  `formkl {
+    {
+      sections: [
+        {
+          title: "Reverting location keywords",
+          fields: [
+            {
+              type: "select",
+              options: [],
+              fetchUrl: "/api/company",
+              label: "Target Company",
+              require: true,
+            },
+            {
+              type: "text",
+              label: "From keyword",
+              require: true,
+            },
+            {
+              type: "text",
+              label: "To keyword",
+              require: true,
+            },
+            {
+              type: "switch",
+              label: "Force update",
+              require: false,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  [
+    `formkl {
     multiple "Target" includes {
       require "Target Company" select url("/api/company");
       require "Group" select url("https://someapi.com/api/group");
@@ -74,4 +381,49 @@ module.exports = [
     }
   }
   `,
+    {
+      sections: [
+        {
+          title: "Target",
+          multiple: true,
+          fields: [
+            {
+              type: "select",
+              options: [],
+              fetchUrl: "/api/company",
+              label: "Target Company",
+              require: true,
+            },
+            {
+              type: "select",
+              options: [],
+              fetchUrl: "https://someapi.com/api/group",
+              label: "Group",
+              require: true,
+            },
+            {
+              type: "switch",
+              label: "From unassign location",
+              require: false,
+            },
+          ],
+        },
+        {
+          title: "Date",
+          fields: [
+            {
+              type: "date",
+              label: "From date",
+              require: true,
+            },
+            {
+              type: "date",
+              label: "To date",
+              require: true,
+            },
+          ],
+        },
+      ],
+    },
+  ],
 ];
