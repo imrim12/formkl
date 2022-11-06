85daebaa29596fbf22285e0b4388d534d7cba31b
bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2022-11-07 01:12:47 +0700

    init universal adapter

---
+++ packages/adapters/universal/src/components/SectionNode.tsx
@@ -0,0 +1,37 @@
+import { Section } from "@formkl/shared";
+import { SectionEvent } from "../types/section-event.type";
+import { FieldEvent } from "../types/field-event.type";
+
+import FieldNode from "./FieldNode";
+
+type SectionNodeProps = {
+  section: Section;
+  sectionIndex: number;
+  onSectionChange: (payload: SectionEvent) => void;
+};
+
+export default function SectionNode(props: SectionNodeProps) {
+  const { section, sectionIndex, onSectionChange } = props;
+
+  const handleFieldChange = (payload: FieldEvent) => {
+    onSectionChange({
+      section,
+      sectionIndex,
+      ...payload,
+    });
+  };
+
+  return (
+    <section className="section__inner">
+      <div className="section__wrapper">
+        {section.fields.map((field, fieldIndex) => (
+          <FieldNode
+            field={field}
+            fieldIndex={fieldIndex}
+            onFieldChange={(payload) => handleFieldChange(payload)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
---
+++ packages/adapters/universal/src/core/Model.ts
@@ -0,0 +1,123 @@
+import { Formkl, SchemaBase, SchemaFlat } from "@formkl/shared";
+import { Schema } from "./Schema";
+
+export const DefaultValueMap = Object.freeze({
+  number: null,
+  text: null,
+  paragraph: null,
+  date: new Date(),
+  time: new Date(),
+  datetime: new Date(),
+  daterange: [new Date(), new Date()],
+  timerange: [new Date(), new Date()],
+  datetimerange: [new Date(), new Date()],
+  select: null,
+  checkbox: [],
+  radio: null,
+  switch: false,
+});
+
+export class Model {
+  private _formkl: Formkl;
+  private _schema: Schema;
+  private _modelDefault: SchemaBase | SchemaFlat;
+  private _model: SchemaBase | SchemaFlat;
+
+  constructor(formkl: Formkl, schema: Schema, modelDefault?: SchemaBase | SchemaFlat) {
+    this._formkl = formkl;
+    this._schema = schema;
+    this._modelDefault = modelDefault || {};
+
+    // Create the modelSchema
+    const { type, schema: modelSchema } = this._schema.getSchema();
+
+    // Prefill the model with default value
+    if (this._modelDefault) {
+      switch (type) {
+        case "flat":
+          this._model = this._prefillFlatModel(
+            modelSchema as SchemaFlat,
+            this._modelDefault as SchemaFlat,
+          );
+          break;
+        case "base":
+        default:
+          this._model = this._prefillBaseModel(
+            modelSchema as SchemaBase,
+            this._modelDefault as SchemaBase,
+          );
+          break;
+      }
+    } else {
+      this._model = modelSchema;
+    }
+  }
+
+  private _prefillFlatModel(schema: SchemaFlat, modelDefault: SchemaFlat): SchemaFlat {
+    const model: SchemaFlat = schema;
+
+    this._formkl.sections.forEach((section) => {
+      section.fields.forEach((field) => {
+        const prefillValue = modelDefault?.[section.key]?.[field.key];
+
+        const defaultValue =
+          section.multiple || field.multiple
+            ? [DefaultValueMap[field.type]]
+            : DefaultValueMap[field.type];
+
+        if (prefillValue && (section.multiple || field.multiple) && !Array.isArray(prefillValue)) {
+          console.error(
+            "[Formkl Warn]: The field or section is defined as having multiple response but the value is not an array.",
+          );
+        }
+
+        model[section.key] = {
+          ...model[section.key],
+          [field.key]: prefillValue || defaultValue,
+        };
+      });
+    });
+
+    return model;
+  }
+
+  private _prefillBaseModel(schema: SchemaBase, modelDefault: SchemaBase): SchemaBase {
+    const model: SchemaBase = schema;
+
+    this._formkl.sections.forEach((section) => {
+      section.fields.forEach((field) => {
+        const modelField = model.data.find(
+          (i) => i.section === section.key && i.field === field.key,
+        );
+        if (modelField) {
+          const prefillValue = modelDefault?.data?.find(
+            (i) => i.section === section.key && i.field === field.key,
+          )?.value;
+
+          const defaultValue =
+            section.multiple || field.multiple
+              ? [DefaultValueMap[field.type]]
+              : DefaultValueMap[field.type];
+
+          if (
+            prefillValue &&
+            (section.multiple || field.multiple) &&
+            !Array.isArray(prefillValue)
+          ) {
+            console.error(
+              "[Formkl Warn]: The field or section is defined as having multiple response but the value is not an array.",
+            );
+          }
+
+          modelField.value = prefillValue || defaultValue;
+        }
+      });
+    });
+
+    return model;
+  }
+
+  public getModel(): SchemaBase | SchemaFlat {
+    return this._model;
+  }
+}
