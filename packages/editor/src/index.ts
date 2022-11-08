b16fae6fcf8eca9f6b2c240078584ccabaeffd29
bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2022-11-08 23:07:01 +0700

    fix: editor web component lifecycle works differently on vue/react

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -1,4 +1,5 @@
 import { indentWithTab } from "@codemirror/commands";
+import { EditorState } from "@codemirror/state";
 import { EditorViewConfig, keymap } from "@codemirror/view";
 import { EditorView, basicSetup } from "codemirror";
 import { AutoCompleteExtension, LintExtension } from "./extensions";
@@ -13,6 +14,20 @@ type EditorOptions = EditorViewConfig & {
 };
 
 export const createEditor = (options?: EditorOptions): CustomElementConstructor => {
+  // Vue call order:
+  // 1. get value
+  // 3. connectedCallback
+  // 2. set value
+  // 4. get value (on every update)
+
+  // React call order:
+  // 1. get value
+  // 2. attributeChangedCallback
+  // 3. get value
+  // 4. connectedCallback
+  // 5. get value (on every update)
+  // 6. attributeChangedCallback (on every update)
+
   return class FormklEditor extends HTMLElement {
     static get observedAttributes() {
       return ["value"];
@@ -20,71 +35,87 @@ export const createEditor = (options?: EditorOptions): CustomElementConstructor 
 
     private modified = false;
 
-    editor: EditorView | null = null;
+    public editor: EditorView | null = null;
 
-    get value() {
-      return this.editor?.state.doc.toString() || "";
+    public get value() {
+      return this.getContent();
     }
 
-    set value(newValue: string) {
+    public set value(newValue: string) {
       this.setContent(newValue);
+    }
+
+    public connectedCallback() {}
+
+    public attributeChangedCallback(name: string, oldValue: any, newValue: any) {
+      if (name === "value") {
+        this.setContent(newValue);
+      }
     }
 
     constructor() {
       super();
       this.attachShadow({ mode: "open" });
-    }
 
-    setContent(value: string) {
-      this.editor?.dispatch({
-        changes: {
-          from: 0,
-          to: this.editor.state.doc.length,
-          insert: value,
-        },
-      });
-    }
-
-    // Like mounted
-    connectedCallback() {
       EditorView.theme(options?.theme || {}, { dark: Boolean(options?.dark) });
+
+      const wrapper = document.createElement("div");
+      wrapper.id = "formkl__editor";
+
+      this.shadowRoot.appendChild(wrapper);
 
       this.editor = new EditorView({
         ...options,
-        parent: this.shadowRoot as DocumentFragment,
-        doc: this.value,
-        extensions: [
-          basicSetup,
-          keymap.of([indentWithTab]),
-          AutoCompleteExtension,
-          LintExtension,
-          EditorView.domEventHandlers({
-            input: (e) => {
-              const eventInput = new CustomEvent("input", {
-                detail: this.value,
-              });
-              this.dispatchEvent(eventInput);
-
-              this.modified = true;
-            },
-            blur: (e) => {
+        parent: this.shadowRoot.getElementById("formkl__editor"),
+        state: EditorState.create({
+          doc: this.value,
+          extensions: [
+            basicSetup,
+            keymap.of([indentWithTab]),
+            AutoCompleteExtension,
+            LintExtension,
+            EditorView.updateListener.of((viewUpdate) => {
+              const eventInput = new CustomEvent("input", { detail: this.value });
+              const eventFocus = new CustomEvent("focus");
               const eventBlur = new CustomEvent("blur");
-              this.dispatchEvent(eventBlur);
+              const eventChange = new CustomEvent("change");
+              // https://discuss.codemirror.net/t/codemirror-6-proper-way-to-listen-for-changes/2395/11
+              if (viewUpdate.docChanged) {
+                this.dispatchEvent(eventInput);
 
-              if (this.modified) {
-                const eventChange = new CustomEvent("change");
-                this.dispatchEvent(eventChange);
-                this.modified = false;
+                this.modified = true;
               }
-            },
-          }),
-        ].concat(options?.extensions || []),
+              // focus state change
+              if (viewUpdate.focusChanged) {
+                viewUpdate.view.hasFocus
+                  ? this.dispatchEvent(eventFocus)
+                  : this.dispatchEvent(eventBlur);
+
+                if (this.modified && !viewUpdate.view.hasFocus) {
+                  this.dispatchEvent(eventChange);
+
+                  this.modified = false;
+                }
+              }
+            }),
+          ].concat(options?.extensions || []),
+        }),
       });
     }
 
-    attributeChangedCallback(name: string, oldValue: any, newValue: any) {
-      if (name === "value") {
-        this.setContent(newValue);
+    public getContent() {
+      return this.editor?.state.doc.toString() || "";
+    }
+
+    public setContent(content: string) {
+      if (content !== this.getContent()) {
+        this.editor.dispatch({
+          changes: {
+            from: 0,
+            to: this.editor.state.doc.length,
+            insert: content,
+          },
+        });
       }
     }
   };
