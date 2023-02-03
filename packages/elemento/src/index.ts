7f1102506be2fa5c2880abfd5b5b4b3629a3fa88
prd-y-nguyen <y.nguyen@paradox.ai>
2023-02-03 16:25:27 +0700

    feat: add more node fields to elemento theme

--- packages/elemento/src/index.ts
+++ packages/elemento/src/index.ts
@@ -1,6 +1,18 @@
-import { h } from "vue";
+import { defineComponent, h } from "vue";
 import { Theme } from "@formkl/shared";
-import { ElInput, ElButton, ElSelectV2 } from "element-plus";
+import {
+  ElInput,
+  ElButton,
+  ElSelectV2,
+  ElSwitch,
+  ElInputNumber,
+  ElDatePicker,
+  ElTimePicker,
+  ElCheckboxGroup,
+  ElCheckbox,
+  ElRadioGroup,
+  ElRadio,
+} from "element-plus";
 
 import "element-plus/dist/index.css";
 
@@ -10,7 +22,41 @@ const themeConfig: Theme = {
   vNodeFields: {
     text: h(ElInput),
     paragraph: h(ElInput, { type: "textarea" }),
+    switch: h(ElSwitch),
+    number: h(ElInputNumber),
+    date: h(ElDatePicker),
+    time: h(ElTimePicker),
+    datetime: h(ElDatePicker, { type: "datetime" }),
+    daterange: h(ElDatePicker, { isRange: true }),
+    timerange: h(ElTimePicker, { isRange: true }),
+    datetimerange: h(ElDatePicker, { type: "datetime", isRange: true }),
     select: h(ElSelectV2),
+    checkbox: defineComponent({
+      props: {
+        options: Array,
+      },
+      setup(props, { attrs }) {
+        return () =>
+          h(
+            ElCheckboxGroup,
+            Object.assign({}, props, attrs),
+            props.options.map((option: any) => h(ElCheckbox, { label: option }, option)),
+          );
+      },
+    }),
+    radio: defineComponent({
+      props: {
+        options: Array,
+      },
+      setup(props, { attrs }) {
+        return () =>
+          h(
+            ElRadioGroup,
+            Object.assign({}, props, attrs),
+            props.options.map((option: any) => h(ElRadio, { label: option }, option)),
+          );
+      },
+    }),
   },
   vNodeComponents: {
     addSection: h(ElButton, () => "Add section"),
